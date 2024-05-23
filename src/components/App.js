d1c7d421b734dfcb21d162aeefa62ce6e7af3d9c
Sourabh Bansotiya <77042105+developer-sourabh28@users.noreply.github.com>
2024-05-23 18:06:24 +0000

    changes made

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,8 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react"
+import { createRoot } from "react-dom/client"
+
+<p>{"Now I can render any React component on any DOM node I want using ReactDOM.render"}</p>
+
+const app = document.getElementById("root");
+const root = createRoot(app);
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import { create } from "cypress/types/lodash";
+import { createRoot } from "react-dom/client";
 
-
+const root = createRoot(document.getElementById("root"));
+root.render(<App/>)
 
